437b31809f61871670319af8182b958f0c454518
Fernanda <fernandalosneves@gmail.com>
2022-07-27 10:28:32 -0300

    blog inicializado home .tsx e css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Pokemãons </h1>
+           <img src='https://lh3.googleusercontent.com/9FHOk79iiGEisBJxkU9smRi8CUKagEkt_yl7T7z9mEBHypSg5sblsGkv1YOxj-4vCpVbYUeo7dC6q2rxiHn9fNlcBxXGabLd7RpsNC6MHrwCRw=e365-w567' alt="pokemon" className="img"></img>
+        </>
+    );
+}
+
+export default Home;
